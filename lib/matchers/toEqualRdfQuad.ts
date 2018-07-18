c0dc417e7b626b53d9654a82d74514e81f90cf01
Ruben Taelman <ruben.taelman@ugent.be>
2018-07-18 10:35:52 +0200

    Add toEqualRdfQuad

---
+++ lib/matchers/toEqualRdfQuad.ts
@@ -0,0 +1,32 @@
+import * as RDF from "rdf-js";
+import {quadToStringQuad} from "rdf-string";
+import M from "./toEqualRdfTerm";
+
+export default {
+  toEqualRdfQuad(received: RDF.Quad, actual: RDF.Quad) {
+    const s = M.toEqualRdfTerm(received.subject, actual.subject);
+    if (!s.pass) {
+      return s;
+    }
+    const p = M.toEqualRdfTerm(received.predicate, actual.predicate);
+    if (!p.pass) {
+      return p;
+    }
+    const o = M.toEqualRdfTerm(received.object, actual.object);
+    if (!o.pass) {
+      return o;
+    }
+    const g = M.toEqualRdfTerm(received.graph, actual.graph);
+    if (!g.pass) {
+      return g;
+    }
+
+    return {
+      message: () => `expected
+  ${JSON.stringify(quadToStringQuad(received))}
+not to equal
+  ${JSON.stringify(quadToStringQuad(actual))}`,
+      pass: true,
+    };
+  },
+};
