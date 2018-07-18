1934281afd26252b452cf978e2731a777327f64d
Ruben Taelman <ruben.taelman@ugent.be>
2018-07-18 10:35:24 +0200

    Add toEqualRdfTerm

---
+++ lib/matchers/toEqualRdfTerm.ts
@@ -0,0 +1,30 @@
+import * as RDF from "rdf-js";
+import {termToString} from "rdf-string";
+
+function fail(received: RDF.Term, actual: RDF.Term) {
+  return {
+    message: () => `expected ${termToString(received)} and ${termToString(actual)} to be equal`,
+    pass: false,
+  };
+}
+
+function succeed(received: RDF.Term, actual: RDF.Term) {
+  return {
+    message: () => `expected ${termToString(received)} and ${termToString(actual)} not to be equal`,
+    pass: true,
+  };
+}
+
+export default {
+  toEqualRdfTerm(received: RDF.Term, actual: RDF.Term) {
+    if (received.termType !== actual.termType) {
+      return fail(received, actual);
+    }
+
+    if (received.termType !== 'BlankNode' && !received.equals(actual)) {
+      return fail(received, actual);
+    }
+
+    return succeed(received, actual);
+  },
+};
